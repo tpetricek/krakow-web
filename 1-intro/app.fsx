470d8e5ada0fac51950fee61b6f40aa4d4905736
Tomas Petricek <tomas@tomasp.net>
2016-02-17 08:43:25 +0100

    Add all code

---
+++ 1-intro/app.fsx
@@ -0,0 +1,108 @@
+#r "System.Xml.Linq.dll"
+#r "packages/Suave/lib/net40/Suave.dll"
+#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+#r "packages/DotLiquid/lib/NET40/DotLiquid.dll"
+#r "packages/Suave.DotLiquid/lib/net40/Suave.DotLiquid.dll"
+open System
+open System.Web
+open System.IO
+open FSharp.Data
+open Suave
+open Suave.Web
+open Suave.Http
+open Suave.Filters
+open Suave.Operators
+
+DotLiquid.setTemplatesDir (__SOURCE_DIRECTORY__ + "/templates")
+
+// -------------------------------------------------------------------------------------------------
+// STEP #1: Hello world and composing web parts
+// -------------------------------------------------------------------------------------------------
+
+let app_1 = 
+  Successful.OK "Hello world!"
+
+let app_2 =
+  choose
+    [ path "/" >=> Successful.OK "See <a href=\"/add/40/2\">40 + 2</a>"
+      pathScan "/add/%d/%d" (fun (a,b) -> Successful.OK(string (a + b)))
+      RequestErrors.NOT_FOUND "404: Not found" ]
+
+// TODO: Write an echo service that will print your name
+// For example, given "/echo/Tomas", it should print <h1>Hello Tomas</h1>
+// To accept strings in the path, you can use "%s"
+
+// -------------------------------------------------------------------------------------------------
+// STEP #2: Records and templating with DotLiquid
+// -------------------------------------------------------------------------------------------------
+
+type News = 
+  { Title : string 
+    Description : string
+    Link : string }
+
+// Create a value of type News
+let newsItem id =
+  { Title = sprintf "Suave workshop %d!" id
+    Description = "Suave workshop is happening today, do not miss it!"
+    Link = "http://www.fsharpworks.com" }
+    
+// Working with collections using higher-order functions
+let demo3 = 
+  [ 1 .. 10 ]
+  |> Seq.map (fun i -> newsItem i)
+  |> List.ofSeq
+
+let app_3 = 
+  DotLiquid.page "home.html" demo3
+
+// Working with collections using list comprehensions
+let demo4 = 
+  [ for i in 1 .. 10 do
+      yield newsItem i ]
+  
+let app_4 = 
+  DotLiquid.page "home.html" demo4
+
+// -------------------------------------------------------------------------------------------------
+// STEP #3: Calling HTTP based REST services
+// -------------------------------------------------------------------------------------------------
+
+// TODO: Show weather forecast for the next few days:
+// "http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&q=Krakow&APPID=cb63a1cf33894de710a1e3a64f036a27"
+
+type RssFeed = XmlProvider<"http://fpish.net/rss/blogs/tag/1/f~23">
+
+let rss = RssFeed.GetSample() 
+  
+let news = 
+  [ for item in rss.Channel.Items do
+      yield newsItem ]
+
+let app_5 = 
+  DotLiquid.page "home.html" news
+
+// -------------------------------------------------------------------------------------------------
+// ENTRY POINT: Choose which of the demos to run here!
+// -------------------------------------------------------------------------------------------------
+
+let app = app_1
+
+
+// -------------------------------------------------------------------------------------------------
+// To run the web site, you can use `build.sh` or `build.cmd` script, which is nice because it
+// automatically reloads the script when it changes. But for debugging, you can also use run or
+// run with debugger in VS or XS. This runs the code below.
+// -------------------------------------------------------------------------------------------------
+
+#if INTERACTIVE
+#else
+let cfg =
+  { defaultConfig with
+      bindings = [ HttpBinding.mkSimple HTTP  "127.0.0.1" 8011 ]
+      homeFolder = Some __SOURCE_DIRECTORY__ }
+let _, server = startWebServerAsync cfg app
+Async.Start(server)
+System.Diagnostics.Process.Start("http://localhost:8011")
+System.Console.ReadLine() |> ignore
+#endif
---
+++ 2-pollz/code/results.fs
@@ -0,0 +1,122 @@
+﻿module Pollz.Results
+open Pollz.Data
+open FSharp.Data
+open Suave
+open Suave.Http
+open Suave.Filters
+open XPlot.GoogleCharts
+
+// ----------------------------------------------------------------------------
+// STEP #2: Implement code to display the poll results. To do this, you first
+// need to complete STEP #2 in `data.fs`. Then you'll need to implement the
+// `getResults` function below using the `Data.loadPoll` helper and using
+// `Data.votes` (which contains the votes)
+// ----------------------------------------------------------------------------
+
+/// For each answer, we show the answer and the number of votes
+type Answer = 
+  { Option : string 
+    Votes : int }
+
+/// The model contains basic poll info and a sequence of results
+type Poll =
+  { Title : string
+    Question : string 
+    Results : seq<Answer> }
+
+let ``TUTORIAL DEMO #2`` () = 
+  // Say we have a map (which is what `Data.votes` will give you)
+  let testVotes = Map.ofSeq [ ("testpoll", [0;1;0;0;0;1]) ]
+  // You can get the data for a given key using `TryFind`
+  let optVotes = testVotes.TryFind "testpoll"
+  // And you can provide a default value using `defaultArg`
+  // (by default, we record no votes)
+  let votes = defaultArg optVotes []
+
+  // To count how many votes for a certain option are there, you
+  // then need to count how many times a certain value occurs in 
+  // the votes list. You can do that using `List.filter` and `List.length`
+  [ 0;1;0;1;0 ]
+  |> List.filter (fun x -> x = 0)
+  |> List.length
+
+
+// You can delee the `TUTORIAL DEMO #1` function and implement the following:
+let getResults pollName =
+  { Title = "Testing"
+    Question = "What is your favourite color?"
+    Results = 
+        [ { Option = "Yo"; Votes = 2 }
+          { Option = "Nae"; Votes = 5 } ] }
+
+let part1 =
+  pathScan "/results/%s" (fun name ->
+    DotLiquid.page "results.html" (getResults name) )
+
+// ----------------------------------------------------------------------------
+// STEP #6: Plot the results on a chart. To do this add `Chart` property of 
+// type `string` to the `Poll` (and modify `results.html` to display this).
+// To get the HTML, just call `Chart.Pie` (or whichever chart you like) and
+// get the `InlineHtml`.
+// ----------------------------------------------------------------------------
+
+let ``TUTORIAL DEMO #6`` () = 
+  // Make a chart using XPlot
+  let chart = Chart.Pie [ ("Good",12); ("Bad",1) ]
+  // Get the HTML of the chart
+  chart.InlineHtml
+
+
+// ----------------------------------------------------------------------------
+// STEP #9: Sending live updates with WebSockets! The `socket { .. }`
+// computation is like `async { .. }` except that you can send messages
+// to the client using `webSocket.send`. Here, we want to send JSON
+// updates with new data when someone votes. To do this, we'll also need
+// to modify `data.fs` to trigger an event when voting happens.
+// ----------------------------------------------------------------------------
+
+
+open Suave.Utils
+open Suave.Sockets
+open Suave.WebSocket
+open Suave.Sockets.Control
+
+// Defines a type `LiveUpdate.Root` that you can use to produce JSON values
+type LiveUpdate = JsonProvider<"""
+  { "chart":"<div>...</div>", "votes":[1,2,3] }""">
+
+let ``TUTORIALO DEMO #9`` () =
+  // Create a new JSON value
+  let obj = LiveUpdate.Root("<div>New HTML</div>", [| 10; 10 |])
+  // Use F# Data to format it as string
+  obj.JsonValue.ToString()
+  
+
+let sendLiveUpdates pollName (webSocket:WebSocket) ctx = socket {
+  while true do
+    // Once you add `votesUpdated` event to `data.fs`, you can wait
+    // until it is triggered using the following:
+    //
+    //   let! update = 
+    //     votesUpdated.Publish 
+    //     |> Async.AwaitEvent |> SocketOp.ofAsync
+    //
+    // This will give you the name of the poll that has been changed.
+    // If this is the same as current `pollName`, then create a JSON
+    // string with the new data (see DEMO #9) and send it to the client!
+    // (You can reuse `getResults` to get the data).
+
+    let update = "Hello client!"
+    do! webSocket.send Text (ASCII.bytes update) true 
+
+    // Let's sleep for 10 seconds before another update
+    // (You will not need this once you implement the event!)
+    do! Async.Sleep(10000) |> SocketOp.ofAsync }
+  
+let part2 = 
+  pathScan "/results/%s/updates" (fun name ->
+    
+    ServerErrors.INTERNAL_ERROR "Not implemented...."
+    // handShake (sendLiveUpdates name)
+
+  ) 
